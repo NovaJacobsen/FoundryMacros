b220a342537482286f9b778f55de8933a2fb592d
Nova <i.am.nova.jacobsen@gmail.com>
2021-06-05 08:28:34 +0200

    Suppress resolution warning in game

--- src/nova/hooks/index.ts
+++ src/nova/hooks/index.ts
@@ -3,6 +3,9 @@ import * as nova from "..";
 export const registerHooks = () => {
   Hooks.once("ready", () => {
     //@ts-ignore
-    window.novaAPI = nova
+    window.novaAPI = nova;
+    setTimeout(() =>
+      $(".notification:contains(requires a minimum screen resolution)").hide()
+    );
   });
 };
